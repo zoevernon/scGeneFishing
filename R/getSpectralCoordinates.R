05ef03905db6c84f2310dfa02bb82f974fee499b
zoevernon <zoe_vernon@berkeley.edu>
2021-03-30 21:20:22 -0700

    initial functions

---
+++ R/getSpectralCoordinates.R
@@ -0,0 +1,56 @@
+#' getSpectralCoordinates
+#' 
+#' Internal function to compute spectral coordinates from similarity matrix
+#'
+#' @param A similarity matrix 
+#' @param n_eigen_vectors number of eigen vectors to save for clustering
+#' 
+#' @return List with the following components 
+#' \itemize{
+#'   \item \code{coordinate_matrix} - matrix of spectral coordinates for each 
+#'   gene
+#'   \item \code{eigen_values} - eigen values corresponding to columns of 
+#'   \code{coordinate_matrix}.
+#' }
+#' 
+#' @examples
+#' 
+
+getSpectralCoordinates <- function(A, n_eigen_vectors = 0){ 
+  diag(A) <- 0
+  A <- abs(A)
+  d <- apply(A, 1, sum)
+  I <- diag(1, nrow = nrow(A))
+  L <- I - diag(1/sqrt(d)) %*% A %*% diag(1/sqrt(d))
+  
+  # L is positive semi-definite and have n non-negative real-valued eigenvalues
+  tmp <- eigen(L)
+  eigen_values <- tmp$values[length(tmp$values):1]
+  eigen_vectors <- tmp$vectors[, length(tmp$values):1]
+  
+  # automaticaly choose number of eigen-vectors
+  q1 <- quantile(eigen_values)[2]
+  q3 <- quantile(eigen_values)[4]
+  low <- q1 - 3 * (q3 - q1)
+  up <- q1 + 3 * (q3 - q1)
+  
+  # pick out eigen-values that are relatively small
+  k <- sum(eigen_values < low) 
+  
+  if(n_eigen_vectors > 1){
+    k <- n_eigen_vectors
+  }
+  if(k == 1){
+    return(list(eigen_values = eigen_values, n_eigen_vectors = 1))
+  }else{
+    # pick out the eigen-vectors associated with the k smallest non-zero 
+    # eigen-values
+    coordinate_matrix <- eigen_vectors[, 2:(k + 1)]
+    rownames(coordinate_matrix) <- rownames(A)
+    colnames(coordinate_matrix) <- paste('eigen', 1:(k), sep = "-")
+    
+    return(list(coordinates = coordinate_matrix,
+                eigen_values = eigen_values,
+                n_eigen_vectors = k))
+  }
+}
