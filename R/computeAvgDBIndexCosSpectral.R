4f5db068122b934798e3a0db289829bd196aa7e3
zoevernon <zoe_vernon@berkeley.edu>
2021-08-20 17:36:47 -0400

    added tryCatch

--- R/computeAvgDBIndexCosSpectral.R
+++ R/computeAvgDBIndexCosSpectral.R
@@ -14,37 +14,41 @@ computeAvgDBIndexCosSpectral <- function(bait_genes, exp_mat, method,
   all_genes  <- rownames(exp_mat)       
   pool_genes <- setdiff(all_genes, bait_genes)
   
-  # and across different samples of random genes
   bait_tightness <- 
     foreach(i = 1:n_rounds, .combine = c) %dopar% {
-      # sample random genes 
-      rand_genes <- sample(pool_genes, 
-                           size = length(bait_genes) * alpha)
-      
-      # get correlation matrix for those genes
-      cor_subset <- cosineSimMatrix(exp_mat[c(bait_genes, rand_genes), ] %>% 
-                                      as.matrix())
-      
-      # get coordinates for spectral clustering
-      rs <- getSpectralCoordinates(cor_subset, k)
-      
-      coordinates <- rs[['coordinates']]
-      
-      # compute modified DB index, first compute centroids of clusters
-      centroids <- rbind(Rfast::colMedians(coordinates[bait_genes, ]),
-                         Rfast::colMedians(coordinates[rand_genes, ]))
-      
-      
-      avg_bait_dist <- Rfast::Dist(coordinates[bait_genes, ]) %>%
-        as.matrix() %>%
-        gdata::upperTriangle() %>%
-        median()
-      
-      # compute index
-      avg_bait_dist / (Rfast::Dist(centroids, vector = TRUE) %>% as.numeric())
+      result <- tryCatch({
+        # sample random genes 
+        rand_genes <- sample(pool_genes, 
+                             size = length(bait_genes) * alpha)
+        
+        # get correlation matrix for those genes
+        cor_subset <- cosineSimMatrix(exp_mat[c(bait_genes, rand_genes), ] %>% 
+                                        as.matrix())
+        
+        # get coordinates for spectral clustering
+        rs <- getSpectralCoordinates(cor_subset, k)
+        
+        coordinates <- rs[['coordinates']]
+        
+        # compute modified DB index, first compute centroids of clusters
+        centroids <- rbind(Rfast::colMedians(coordinates[bait_genes, ]),
+                           Rfast::colMedians(coordinates[rand_genes, ]))
+        
+        
+        avg_bait_dist <- Rfast::Dist(coordinates[bait_genes, ]) %>%
+          as.matrix() %>%
+          gdata::upperTriangle() %>%
+          median()
+        
+        # compute index
+        avg_bait_dist / (Rfast::Dist(centroids, vector = TRUE) %>% as.numeric())
+      },
+      error = function(err) {
+        NA
+      })
+      result
     }
   
-  
-  return(bait_tightness)
+  return(bait_tightness[!is.na(bait_tightness)])
 }
 
--- R/computeAvgDBIndexCosUMAP.R
+++ R/computeAvgDBIndexCosUMAP.R
@@ -17,32 +17,39 @@ computeAvgDBIndexCosUMAP <- function(bait_genes, exp_mat, method,
   # and across different samples of random genes
   bait_tightness <- 
     foreach(i = 1:n_rounds, .combine = c) %dopar% {
-      # sample random genes 
-      rand_genes <- sample(pool_genes, 
-                           size = length(bait_genes) * alpha)
-      
-      # get correlation matrix for those genes
-      cor_subset <- cosineSimMatrix(exp_mat[c(bait_genes, rand_genes), ] %>% 
-                                      as.matrix())
-      
-      # get coordinates for spectral clustering
-      coordinates <- getUMAPCoordinates(cor_subset, k, n_neighbors)
-      
-      # compute modified DB index, first compute centroids of clusters
-      centroids <- rbind(Rfast::colMedians(coordinates[bait_genes, ]),
-                         Rfast::colMedians(coordinates[rand_genes, ]))
-      
-      
-      avg_bait_dist <- Rfast::Dist(coordinates[bait_genes, ]) %>%
-        as.matrix() %>%
-        gdata::upperTriangle() %>%
-        median()
-      
-      # compute index
-      avg_bait_dist / 
-        sqrt((Rfast::Dist(centroids, vector = TRUE) %>% as.numeric()))
-    } 
+      result <- tryCatch({
+        # sample random genes 
+        rand_genes <- sample(pool_genes, 
+                             size = length(bait_genes) * alpha)
+        
+        # get correlation matrix for those genes
+        cor_subset <- cosineSimMatrix(exp_mat[c(bait_genes, rand_genes), ] %>% 
+                                        as.matrix())
+        
+        # get coordinates for spectral clustering
+        coordinates <- getUMAPCoordinates(cor_subset, k, n_neighbors)
+        
+        # compute modified DB index, first compute centroids of clusters
+        centroids <- rbind(Rfast::colMedians(coordinates[bait_genes, ]),
+                           Rfast::colMedians(coordinates[rand_genes, ]))
+        
+        
+        avg_bait_dist <- Rfast::Dist(coordinates[bait_genes, ]) %>%
+          as.matrix() %>%
+          gdata::upperTriangle() %>%
+          median()
+        
+        # compute index
+        avg_bait_dist / 
+          sqrt((Rfast::Dist(centroids, vector = TRUE) %>% as.numeric()))
+      },
+      error = function(err) {
+        NA
+      })
+      result
+    }
   
-  return(bait_tightness)
+  
+  return(bait_tightness[!is.na(bait_tightness)])
 }
 
